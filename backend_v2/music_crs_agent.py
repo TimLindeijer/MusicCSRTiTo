175ecc8970301c35660abe882372e6629f0c28e8
Tim Laptop <tlindeijer@gmail.com>
2024-10-02 15:59:52 +0200

    removed old files

--- backend_v2/music_crs_agent.py
+++ backend_v2/music_crs_agent.py
@@ -1,10 +1,12 @@
-# music_crs_agent.py
 from dialoguekit.core.annotated_utterance import AnnotatedUtterance
 from dialoguekit.core.dialogue_act import DialogueAct
 from dialoguekit.core.utterance import Utterance
 from dialoguekit.participant.agent import Agent
 from dialoguekit.participant.participant import DialogueParticipant
+import logging
 
+logging.basicConfig(level=logging.INFO)  # Set the level to INFO
+logger = logging.getLogger(__name__)  # Create a logger
 class MusicCRSAgent(Agent):
     def __init__(self, id: str):
         super().__init__(id)
@@ -29,6 +31,8 @@ class MusicCRSAgent(Agent):
     def receive_utterance(self, utterance: Utterance) -> None:
         """Handles user commands to manage the playlist."""
         user_input = utterance.text.lower()
+        logger.info(f"utterance received {utterance}")
+        
 
         if "add" in user_input:
             song = user_input.split("add")[-1].strip()
@@ -40,4 +44,15 @@ class MusicCRSAgent(Agent):
             if song in self.playlist:
                 self.playlist.remove(song)
                 response = f"Removed '{song}' from your playlist."
+        elif "list" in user_input:
+            response = "'"+"', '".join(self.playlist)+ "'"
+
+        else:
+            response = f"Message not understood."
+        
+        response = AnnotatedUtterance(
+            "(MusicCRS) " + response,
+            participant=DialogueParticipant.AGENT,
+        )
+        self._dialogue_connector.register_agent_utterance(response)
    
